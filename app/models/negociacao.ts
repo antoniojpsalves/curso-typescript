6dad894614743f20f259790fbc9b0c41d3b228bb
Antonio <antonio.jpsalves@gmail.com>
2022-05-28 21:55:48 -0300

    Checkpoint ajustes e melhorias no código

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,16 +1,19 @@
 export class Negociacao {
-    private _data: Date;
-    private _quantidade: number;
-    private _valor: number;
 
-    constructor(data: Date, quantidade: number, valor: number) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
+    constructor(
+        private _data: Date, 
+        private _quantidade: number, 
+        private _valor: number
+    ) {}
 
     get data(): Date {
-        return this._data;
+        /**
+         * Dava pra manipular a data utilizando métodos de setDate()
+         * Retornando uma nova referência "identica" defendemos a alteração do nosso registro
+         */
+        const data = new Date(this._data.getTime());
+
+        return data;
     }
 
     get quantidade(): number {
